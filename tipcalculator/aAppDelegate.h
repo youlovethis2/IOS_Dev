ed4c4e5738b73f6c7ffc61b8360c0aa96cd670db
shangqing <shangqing@yahoo-inc.com>
2015-01-21 10:30:31 -0800

    Initial Commit

---
+++ tipcalculator/aAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  aAppDelegate.h
+//  tipcalculator
+//
+//  Created by Shangqing Zhang on 1/21/15.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface aAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipcalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by Shangqing Zhang on 1/21/15.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "aAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([aAppDelegate class]));
+    }
+}

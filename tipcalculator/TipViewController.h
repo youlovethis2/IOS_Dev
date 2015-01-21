e89504439a5cae5befb50cd89b5073749f404c40
shangqing <shangqing@yahoo-inc.com>
2015-01-21 14:29:14 -0800

    first commit

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Shangqing Zhang on 1/21/15.
+//  Copyright (c) 2015 Shangqing Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
---
+++ tipcalculator/TipViewController.m
@@ -0,0 +1,38 @@
+//
+//  TipViewController.m
+//  tipcalculator
+//
+//  Created by Shangqing Zhang on 1/21/15.
+//  Copyright (c) 2015 Shangqing Zhang. All rights reserved.
+//
+
+#import "TipViewController.h"
+
+@interface TipViewController ()
+
+@end
+
+@implementation TipViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
